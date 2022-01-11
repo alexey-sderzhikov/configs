ca0e9b76499b0b2f3b36dee3f991f8f7b8e31cba
aosderzhikov <alexey.sderzhikov@bergen.tech>
2022-01-11 13:43:24 +0300

    initial commit

---
+++ polybar/launch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# kill all polybar instances
+killall -q polybar
+
+# wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# launch polybar, using default config location 
+# ~/.config/polybar/config
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mybar &
+  done
+else
+  polybar --reload mybar &
+fi
+
+echo "Polybar launched..."
---
+++ some.go
@@ -0,0 +1,5 @@
+package main
+
+funcdf main() {
+	fmt.Print("loh")
+}
